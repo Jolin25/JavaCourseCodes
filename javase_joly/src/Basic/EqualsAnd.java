426fa7303b090ffa4d09868be5b3b288346b29b5
jrl <584189010@qq.com>
2021-12-31 18:19:34 +0800

    比较是否相等

--- javase_joly/src/Basic/EqualsAnd.java
+++ javase_joly/src/Basic/EqualsAnd.java
@@ -42,42 +42,14 @@ public class EqualsAnd {
         System.out.println("\nInteger i = 128;\n" +
                 "int j = 128;\n" +
                 "i == j ? {}" + (i == j)); //true
-        int high = IntegerCache.high;
+        assert 128 >= 127;
+        // intern()方法，对字符串进行驻留
+        String a1 = new String("a").intern();
+        boolean b1 = a1 == "a";
+        String b2 = new String("b");
+        System.out.println(b1);
+        System.out.println(b2 == "b");
     }
 
-    private static class IntegerCache {
-        static final int low = -128;
-        static final int high;
 
-
-        static {
-            System.out.println("if run this code?");
-            // high value may be configured by property
-            int h = 127;
-            String integerCacheHighPropValue =
-                    sun.misc.VM.getSavedProperty("java.lang.Integer.IntegerCache.high");
-            if (integerCacheHighPropValue != null) {
-                try {
-                    int i = parseInt(integerCacheHighPropValue);
-                    i = Math.max(i, 127);
-                    // Maximum array size is Integer.MAX_VALUE
-                    h = Math.min(i, Integer.MAX_VALUE - (-low) - 1);
-                } catch (NumberFormatException nfe) {
-                    // If the property cannot be parsed into an int, ignore it.
-                }
-            }
-            high = h;
-
-
-            Integer[] cache = new Integer[(high - low) + 1];
-            int j = low;
-            for (int k = 0; k < cache.length; k++) {
-                cache[k] = new Integer(j++);
-            }
-
-
-            // range [-128, 127] must be interned (JLS7 5.1.7)
-            assert IntegerCache.high >= 127;
-        }
-    }
 }
